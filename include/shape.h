5d959936c81a2845782c69ee579cdaffc0889c27
Haoran Wang <hrwangchengdu@hotmail.com>
2017-11-18 16:56:45 -0500

    [master] Update tensor & shape interfaces.

--- include/shape.h
+++ include/shape.h
@@ -6,16 +6,21 @@ namespace MLLib {
 class shape {
  public:
   shape(size_t d0, size_t d1, size_t d2):
-    shape_(make_tuple(d0, d1, d2)) {
+    shape_(make_tuple(d0, d1, d2)), total_(d0 * d1 * d2) {
   }
 
   size_t getDim(int k) {
-   assert(k <= 3);
-   return std::get<k>(shape_);
+    assert(k <= 3);
+    return std::get<k>(shape_);
+  }
+
+  size_t getTotal() {
+    return total_;
   }
   
  private:
   std::tuple<size_t, size_t, size_t> shape_;  
+  size_t total_;
 }
 
 }
--- include/tensor.h
+++ include/tensor.h
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 
-class tensor {
+#include "shape.h"
+
+namespace MLLib {
+
+class Tensor {
 public:
   // matrix M * N
-  tensor(int m, int n, int k) {
-    ptr_ = (float*) calloc(m * n * k * sizeof(float));
+  Tensor(shape s):shape_(s) {
+    ptr_ = (float*) calloc(shape_.getTotal() * sizeof(float));
+  }
+
+  tuple<> getShape() {
   }
 
 private:
+  shape shape_;
   float *ptr_;
 };
+
+}
