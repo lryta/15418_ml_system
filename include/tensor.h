5a82c0576c1b194104d1942bc6942b2b5a034bf3
Haoran Wang <hrwangchengdu@hotmail.com>
2017-11-18 16:01:27 -0500

    [master] Attach header.

--- include/tensor.h
+++ include/tensor.h
@@ -1,2 +1,12 @@
-class vector {
+#include <stdlib.h>
+
+class tensor {
+public:
+  // matrix M * N
+  tensor(int m, int n, int k) {
+    ptr_ = (float*) calloc(m * n * k * sizeof(float));
+  }
+
+private:
+  float *ptr_;
 };
