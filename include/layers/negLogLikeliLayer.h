fc3f44ea75dc4598f7c6ecddffae9758af42d350
Haoran Wang <hrwangchengdu@hotmail.com>
2017-11-26 16:43:08 -0500

    [master] Add neg-loglikelihood layer.

---
+++ include/layers/negLogLikeliLayer.h
@@ -0,0 +1,33 @@
+#include "tensor.h"
+#include "layer.h"
+
+namespace TinyML {
+
+class negLogLikeliLayer:LossLayer {
+ public:
+  negLogLikeliLayer(vecotr<shape> ins):LossLayer(vecotr<shape> ins),
+    correctlyRecognizedNum_(0), loss_(0), predict_(ins[0]) 
+  {}
+
+  vector<Tensor&> getParam() {
+    return {};
+  }
+
+  void initIntermediateState(vector<shape> &ins, vector<shape> &ous) {
+  }
+
+  float getLoss() {
+    return loss_;
+  }
+
+  int correctlyRecognizedDataNum() {
+    return correctlyRecognizedNum_;
+  }
+
+ private:
+  int correctlyRecognizedNum_;
+  float loss_;
+  tensor predict_;
+};
+
+}
---
+++ src/layers/negLogLikeliLayer.cpp
@@ -0,0 +1,30 @@
+#include <math.h>
+#include "negLogLikeliLayer.h"
+
+namespace TinyML {
+
+// ins[0] predict, ins[1] target
+void negLogLikeliLayer::forward(vecotr<Tensor> &ins, vecotr<Tensor> &ous) {
+  assert(ous.size() == 0);
+  auto p_shape = ins[0].getShape();
+  auto t_shape = ins[1].getShape();
+
+  // predict = softmax(in)
+  matrix::softmax(ins[0].getData(), predict_.getData(),
+      ins[0].getDim(1), ins[0].getDim(2));
+
+  // loss_ = neglog(predict, target)
+  loss_ = matrix::negLogLikelihood(predict_.getData(), ins[1].getData(),
+      ins[1].getDim(1), ins[1].getDim(2));
+
+  correctlyRecognizedNum_ = matrix::getCorrectlyRecognized(predict_.getData(), ins[1].getData(),
+      ins[0].getDim(1), ins[0].getDim(2));
+}
+
+void negLogLikeliLayer::backward(vecotr<Tensor> &ins, vecotr<Tensor> &ous) {
+  //  ins_grad = predict_ - target
+  matrix::eleSubtract(predict_.getData(), ins[1].getData(), ins[0].getGrad(),
+      ins[0].getDim(1), ins[0].getDim(2));
+}
+
+}
