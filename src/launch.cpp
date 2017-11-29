0ad3d3ace0667f6a0ecbd5cc98f792505cdbade0
HrWangChengdu <hrwangchengdu@hotmail.com>
2017-11-29 17:39:02 -0500

    [master] Catch thrown char*.

--- src/launch.cpp
+++ src/launch.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
+
 #include "apps/application.h"
 
 int main() {
-  auto app = new TinyML::runMLP();
-  app->run();
-  delete app;
+  try {
+    auto app = new TinyML::runMLP();
+    app->run();
+    delete app;
+  } catch (const char* str) {
+    std::cout << str << "\n";
+  }
   return 0;
 }
