f3a9907b2f580a822a79349e5ababe4971c415c9
Haoran Wang <hrwangchengdu@hotmail.com>
2017-12-08 01:33:34 -0500

    [master] Add ispc namespace for ispc defined func.

--- src/operations/matrixOp.cpp
+++ src/operations/matrixOp.cpp
@@ -28,7 +28,7 @@ namespace matrix{
       size_t gamma_row, size_t dim_num, size_t gamma_col,
       bool t_alpha,  bool t_beta, float a, float b) {
 #ifdef COMPILE_ISPC
-    gemmISPC(alpha, beta, omega, gamma, gamma_row,
+    ispc::gemmISPC(alpha, beta, omega, gamma, gamma_row,
         dim_num, gamma_col, t_alpha, t_beta, a, b);
 #else
     gemmCPU(alpha, beta, omega, gamma, gamma_row,
@@ -46,7 +46,7 @@ namespace matrix{
   void reduceMatrix(float* m, float* v, int row, int col, int reduced_dim) {
     assert(reduced_dim <= 2);
 #ifdef COMPILE_ISPC
-    reduceMatrixISPC(m, v, row, col, reduced_dim);
+    ispc::reduceMatrixISPC(m, v, row, col, reduced_dim);
 #else
     reduceMatrixCPU(m, v, row, col, reduced_dim);
 #endif
@@ -63,7 +63,7 @@ namespace matrix{
   void eleSubtract(float* alpha, float* beta,
       float* omega, int row, int col) {
 #ifdef COMPILE_ISPC
-    eleSubtractISPC(alpha, beta, omega, row, col);
+    ispc::eleSubtractISPC(alpha, beta, omega, row, col);
 #else
     eleSubtractCPU(alpha, beta, omega, row, col);
 #endif
@@ -78,7 +78,7 @@ namespace matrix{
    */
   void sigmoidOp(float* alpha, float* beta, int row, int col) {
 #ifdef COMPILE_ISPC
-    sigmoidOpISPC(alpha, beta, row, col);
+    ispc::sigmoidOpISPC(alpha, beta, row, col);
 #else
     sigmoidOpCPU(alpha, beta, row, col);
 #endif
@@ -93,7 +93,7 @@ namespace matrix{
    */
   void eleSquare(float* alpha, float* beta, int row, int col) {
 #ifdef COMPILE_ISPC
-    eleSquareISPC(alpha, beta, row, col);
+    ispc::eleSquareISPC(alpha, beta, row, col);
 #else
     eleSquareCPU(alpha, beta, row, col);
 #endif
@@ -106,11 +106,14 @@ namespace matrix{
    *   - alpha (row, col)
    */
   void reduceToValue(float* alpha, float* value, int row, int col) {
+/* It seems there's a little error
 #ifdef COMPILE_ISPC
-    reduceToValueISPC(alpha, value, row, col);
+    ispc::reduceToValueISPC(alpha, value, row, col);
 #else
     reduceToValueCPU(alpha, value, row, col);
 #endif
+*/
+    reduceToValueCPU(alpha, value, row, col);
   }
 
   /* linearOp
@@ -123,7 +126,7 @@ namespace matrix{
   void linearOp(float* alpha, float* beta, int row,
       int col, float scale, float bias) {
 #ifdef COMPILE_ISPC
-    linearOpISPC(alpha, beta, row, col, scale, bias);
+    ispc::linearOpISPC(alpha, beta, row, col, scale, bias);
 #else
     linearOpCPU(alpha, beta, row, col, scale, bias);
 #endif
@@ -139,7 +142,7 @@ namespace matrix{
    */
   void multiEle(float* alpha, float* beta, float* omega, int row, int col) {
 #ifdef COMPILE_ISPC
-    multiEleISPC(alpha, beta, omega, row, col);
+    ispc::multiEleISPC(alpha, beta, omega, row, col);
 #else
     multiEleCPU(alpha, beta, omega, row, col);
 #endif
@@ -154,7 +157,7 @@ namespace matrix{
    */
   void multiEleInplace(float* alpha, float* beta, int row, int col) {
 #ifdef COMPILE_ISPC
-    multiEleInplaceISPC(alpha, beta, row, col);
+    ispc::multiEleInplaceISPC(alpha, beta, row, col);
 #else
     multiEleInplaceCPU(alpha, beta, row, col);
 #endif
@@ -169,7 +172,7 @@ namespace matrix{
    */
   void linearOpInplace(float* alpha, float* beta, int row, int col, float a, float b, float c) {
 #ifdef COMPILE_ISPC
-    linearOpInplaceISPC(alpha, beta, row, col, a, b, c);
+    ispc::linearOpInplaceISPC(alpha, beta, row, col, a, b, c);
 #else
     linearOpInplaceCPU(alpha, beta, row, col, a, b, c);
 #endif
@@ -184,7 +187,7 @@ namespace matrix{
    */
   void UpdateWeightWithReg(float* delta, float* weight, int row, int col, float lr, float reg) {
 #ifdef COMPILE_ISPC
-    UpdateWeightWithRegISPC(delta, weight, row, col, lr, reg);
+    ispc::UpdateWeightWithRegISPC(delta, weight, row, col, lr, reg);
 #else
     UpdateWeightWithRegCPU(delta, weight, row, col, lr, reg);
 #endif
@@ -199,7 +202,7 @@ namespace matrix{
    */
   int getCorrectlyRecognized(float* predicts, float* targets, int row, int col) {
 #ifdef COMPILE_ISPC
-    return getCorrectlyRecognizedISPC(predicts, targets, row, col);
+    return ispc::getCorrectlyRecognizedISPC(predicts, targets, row, col);
 #else
     return getCorrectlyRecognizedCPU(predicts, targets, row, col);
 #endif
@@ -215,7 +218,7 @@ namespace matrix{
    */
   void softmax(float* alpha, float* beta, int row, int col) {
 #ifdef COMPILE_ISPC
-    softmaxISPC(alpha, beta, row, col);
+    ispc::softmaxISPC(alpha, beta, row, col);
 #else
     softmaxCPU(alpha, beta, row, col);
 #endif
@@ -232,18 +235,15 @@ namespace matrix{
    */
   float negLogLikelihood(float* predicts, float* targets, int row, int col) {
 #ifdef COMPILE_ISPC
-    return negLogLikelihoodISPC(predicts, targets, row, col);
+    return ispc::negLogLikelihoodISPC(predicts, targets, row, col);
 #else
     return negLogLikelihoodCPU(predicts, targets, row, col);
 #endif
   }
 
   void normalize(float* data, int row, int col, float mean, float std) {
-#ifdef COMPILE_ISPC
-    normalizeISPC(data, row, col, mean, std);
-#else
+    // No ISPC impl
     normalizeCPU(data, row, col, mean, std);
-#endif
   }
 
   std::tuple<float, float> getStdAndMean(float *data, shape sh) {
